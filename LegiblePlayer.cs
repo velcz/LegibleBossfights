f0e1df96a0f493927e76d699a4ac4a54ec88756b
Velcz <snoopymac254@gmail.com>
2025-11-19 15:28:52 -0700

    1.1.2 Language Support

--- LegiblePlayer.cs
+++ LegiblePlayer.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.GameInput;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace LegibleBossfights
@@ -21,26 +22,24 @@ namespace LegibleBossfights
             if (LegibleBossfights.ToggleLineKey.JustPressed)
             {
                 LegibleBossfights.ShowLine = !LegibleBossfights.ShowLine;
-                Main.NewText("Cursor Line: " + getboolname(LegibleBossfights.ShowLine), 100, 240, 100);
+                Main.NewText(Language.GetTextValue("Mods.LegibleBossfights.Messages.ShowLine", getboolname(LegibleBossfights.ShowLine)), 100, 240, 100);
             }
             if (LegibleBossfights.ToggleProjKey.JustPressed)
             {
                 LegibleBossfights.ShowCircles = !LegibleBossfights.ShowCircles;
-                Main.NewText("Enemy Projectile Indicators: " + getboolname(LegibleBossfights.ShowCircles), 100, 240, 100);
+                Main.NewText(Language.GetTextValue("Mods.LegibleBossfights.Messages.ShowCircles", getboolname(LegibleBossfights.ShowCircles)), 100, 240, 100);
             }
             if (LegibleBossfights.ToggleTransparentKey.JustPressed)
             {
                 LegibleBossfights.FadeProjectiles = !LegibleBossfights.FadeProjectiles;
-                Main.NewText("Transparent Friendly Projectiles: " + getboolname(LegibleBossfights.FadeProjectiles), 100, 240, 100);
+                Main.NewText(Language.GetTextValue("Mods.LegibleBossfights.Messages.FadeProjectiles", getboolname(LegibleBossfights.FadeProjectiles)), 100, 240, 100);
             }
             if (LegibleBossfights.ToggleHideWallsKey.JustPressed)
             {
                 LegibleBossfights.HideWalls = !LegibleBossfights.HideWalls;
-
-                if (Main.netMode != Terraria.ID.NetmodeID.Server)
-                    Main.NewText($"Hide Walls: {(LegibleBossfights.HideWalls ? "ON" : "OFF")}", 200, 200, 255);
+                    Main.NewText(Language.GetTextValue("Mods.LegibleBossfights.Messages.HideWalls", getboolname(LegibleBossfights.HideWalls)), 100, 240, 100);
             }
         }
-        public string getboolname(bool b) => b ? "ON" : "OFF";
+        public string getboolname(bool b) => b ? Language.GetTextValue("Mods.LegibleBossfights.Messages.On") : Language.GetTextValue("Mods.LegibleBossfights.Messages.Off");
     }
 }
